3a3b7e2f6e19c9803007b592f7abbc1ca41fe9a9
lefrankleal <falg2@hotmail.com>
2020-10-21 10:55:16 -0500

    select y query para base de datos

--- database.php
+++ database.php
@@ -9,27 +9,21 @@ class Database {
 
     function __construct(){}
 
-    function query($sql = '') {
+    function select($sql = '') {
         $db = new mysqli($this->server, $this->user, $this->password, $this->db) or die('error');
         $query = $db->query($sql);
-        $result = $query->fetch_array(MYSQLI_ASSOC);
-        return $result;
+        $data = [];
+        while ($row = $query->fetch_object()) {
+            $data[] = $row;
+        }
+        $query->free_result();
+        $query->close();
+        return $data;
     }
 
-    /**
-     * Este es un ejemplo para los arreglos
-     */
-    function arreglo() {
-        $array1 = [
-            0 => 'test',
-            1 => 'hola',
-            2 => 'mundo',
-            5 => 'fin'
-        ];
-        $array2 = [
-            'test', 'hola', 'mundo', 'fin', 4, 213123
-        ];
-        var_dump($array1, $array2);
-        echo "<br>$array1[1] $array1[5]<br>";
+    function sql($sql) {
+        $db = new mysqli($this->server, $this->user, $this->password, $this->db) or die('error');
+        $query = $db->query($sql) or die('Problemas de ejecucion '. $db->error );
+        return $query;
     }
 }
